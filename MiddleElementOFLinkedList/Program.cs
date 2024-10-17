050fb7179f669f1a4e0600d3bcaae138d469a943
baish <baish@BAISHNABI>
2024-10-17 13:27:13 +0530

    Add project files.

---
+++ MiddleElementOFLinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+public class MiddleElement {
+    static void Main(string[] args)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.AddLast(1);
+        list.AddLast(2);
+        list.AddLast(3);
+        list.AddLast(4);
+        list.AddLast(5);
+        list.AddLast(6);
+        int a = 0;
+        a = list.Count / 2;
+        Console.WriteLine(list.ElementAt<int>(a));
+    }
+}
+
